49488fa5179c41b4b4746f2a03bc0719e2dedd75
scrollcode4us <scrollcode.4.us@gmail.com>
2022-06-22 23:23:33 +0300

    header cont

--- js/script.js
+++ js/script.js
@@ -1,6 +1,35 @@
-let menuBtn = document.querySelector('.menu-btn');
-let menu = document.querySelector('.menu');
+"use strict"
 
-menuBtn.addEventListener('click', function(){
-	menu.classList.toggle('active');
-});
+let isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+if ( isMobile.any()){
+    document.body.classList.add('_touch');
+    let menuArrows = document.querySelectorAll('.menu__arrow');
+    if(menuArrows.length > 0){
+        for(let i = 0; i < menuArrows.length; i++){
+            let menuArrow = menuArrows[i];
+            menuArrow.addEventListener('click', () => menuArrow.parentElement.classList.toggle('_active'));
+        }
+    }
+} else {
+    document.body.classList.add('_pc');
+}
